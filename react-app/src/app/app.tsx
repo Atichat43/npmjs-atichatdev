8fa54cc436dbeb404a02f3835c462d50462c92d4
Atichat Lappanopakon <atichat.lap@gmail.com>
2024-05-14 16:59:31 +0700

    init lib/react-lib

--- react-app/src/app/app.tsx
+++ react-app/src/app/app.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { ReactLib } from '@atichatdev/react-lib';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      <ReactLib />
       <NxWelcome title="react-app" />
     </div>
   );
