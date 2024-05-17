aa354579bb80dbab42af8b7b55e827ff6eb371cd
Atichat Lappanopakon <atichat.lap@gmail.com>
2024-05-17 12:22:02 +0700

    ci: remove thresholds

--- packages/helpers/vite.config.ts
+++ packages/helpers/vite.config.ts
@@ -58,12 +58,6 @@ export default defineConfig({
       reportsDirectory: '../../coverage/helpers',
       exclude: ['src/index.ts', 'src/**/*.index.ts'],
       provider: 'v8',
-      thresholds: {
-        functions: 80,
-        lines: 80,
-        statements: 80,
-        branches: 80,
-      },
     },
   },
 });
